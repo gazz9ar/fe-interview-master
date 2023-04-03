a955a404e60dcc5ccdd1efcbe75ac73b3c5bd819
Gaspar Santillan <gazz9ar@gmail.com>
2023-04-03 10:20:44 -0300

    empty last played games

--- src/app/pages/components/games/games.component.ts
+++ src/app/pages/components/games/games.component.ts
@@ -1,5 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { gameFilter } from './components/games-filters/games-filters.component';
+import { Observable } from 'rxjs';
+import { Game } from 'src/app/shared';
+import { AppState, selectLastPlayedGames } from 'src/app/state/selectors/Games.selectors';
+import { Store } from '@ngrx/store';
+import { Unsub } from 'src/app/core/Unsubscription/Unsub';
+import { LoadGames } from 'src/app/state/actions/Games.actions';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-games',
@@ -7,11 +14,30 @@ import { gameFilter } from './components/games-filters/games-filters.component';
   styleUrls: ['./games.component.scss'],
   changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class GamesComponent {
+export class GamesComponent extends Unsub implements Unsub{
 
   gamesToLoad:number = 0;
   disableScrolling:boolean = false;
-  constructor() { }
+  showLastPlayedGames:boolean = false;
+  lastPlayedGames$:Observable<Game[]> =  new Observable();
+  
+  constructor(private readonly store:Store<AppState>) { super() }
+
+  ngOnInit(): void {
+		this.getLastPlayedGames();
+	}
+
+	getLastPlayedGames(): void {	
+		this.store.dispatch(LoadGames());
+		this.lastPlayedGames$ = this.store.select(selectLastPlayedGames); 
+		this.lastPlayedGames$
+		.pipe(takeUntil(this.unsubscribe$))
+		.subscribe( games => {  
+			if(games.length > 0) {
+				this.showLastPlayedGames = true;
+			}
+		});    		  
+	}
 
   onScroll() {  
     this.gamesToLoad += 8;
--- src/app/pages/components/home/home.component.ts
+++ src/app/pages/components/home/home.component.ts
@@ -1,4 +1,12 @@
 import { Component, ChangeDetectionStrategy, OnInit} from "@angular/core";
+import { GamesService } from "../games/services/games.service";
+import { Store } from "@ngrx/store";
+import { AppState, selectLastPlayedGames } from "src/app/state/selectors/Games.selectors";
+import { LoadGames } from "src/app/state/actions/Games.actions";
+import { Observable } from "rxjs";
+import { Game } from "src/app/shared";
+import { takeUntil } from "rxjs/operators";
+import { Unsub } from "src/app/core/Unsubscription/Unsub";
 
 const NAME_KEBAB = "app-home";
 
@@ -8,14 +16,29 @@ const NAME_KEBAB = "app-home";
 	host: { class: NAME_KEBAB },
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit{	
+export class HomeComponent extends Unsub implements OnInit{	
 
-	constructor() {
-		
+	lastPlayedGames$:Observable<Game[]> =  new Observable();
+	showLastPlayedGames:boolean = false;
+	
+	constructor(private readonly store:Store<AppState>) {
+		super();
 	}
 
 	ngOnInit(): void {
-		
+		this.getLastPlayedGames();
+	}
+
+	getLastPlayedGames(): void {	
+		this.store.dispatch(LoadGames());
+		this.lastPlayedGames$ = this.store.select(selectLastPlayedGames); 
+		this.lastPlayedGames$
+		.pipe(takeUntil(this.unsubscribe$))
+		.subscribe( games => {  
+			if(games.length > 0) {
+				this.showLastPlayedGames = true;
+			}
+		});    		  
 	}
 
 }
